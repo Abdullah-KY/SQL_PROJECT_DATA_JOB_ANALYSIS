6b8e4100296a242c0d1301581937f54388b2e81d
Abdullah-KY <abdullahkhanyusufi28@gmail.com>
2025-07-25 18:14:37 +0300

    query update

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,5 +1,5 @@
-/* Question: What are the top-paying Data Analyst, Data Scientist, Data Engineer jobs?
-- Identify the top 20 highest paying Data roles that are available remotely.
+/* Question: What are the top-paying Data Analyst jobs?
+- Identify the top 10 highest paying Data Analyst roles that are available remotely.
 - Focuses on job postings with specified salaries (excluding nulls).
 - Why? Highlight the top paying jobs for Data Managing and Manipulating, offering insights
     into current market demand and salary trends */
@@ -17,9 +17,9 @@ FROM
 LEFT JOIN company_dim AS companies 
 ON job_postings.company_id = companies.company_id 
 WHERE
-    job_title_short IN ('Data Analyst', 'Data Scientist', 'Data Engineer') AND
+    job_title_short = 'Data Analyst' AND
     job_location= 'Anywhere' AND 
     salary_year_avg IS NOT NULL
 ORDER BY 
     salary_year_avg DESC
-LIMIT 20;
+LIMIT 10;
